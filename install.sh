2e715d95eed3aede52f25938614ba8856077ea6e
u0_a390 <84712110+pranav10780@users.noreply.github.com>
2023-12-10 10:34:58 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
-#!/data/data/comtermux/files/usr/bin/bash
-chmod +x app-open;
-cp app-open ~/../usr/bin/;
-echo "done";
+#!/data/data/com.termux/files/usr/bin/bash
+echo "Starting program"
+chmod +x app-open
+echo "Setting execute permission for app-open"
+cp app-open ~/../usr/bin/
+echo "Copying app-open to ~/../usr/bin/ done"
