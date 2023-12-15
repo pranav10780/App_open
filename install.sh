15c663cbb04c61106e9a64a5c81bfcdd59ea43fe
u0_a390 <84712110+pranav10780@users.noreply.github.com>
2023-12-15 10:42:12 +0530

    Delete install.sh

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/bash
-echo "Starting program"
-chmod +x app-open
-echo "Setting execute permission for app-open"
-cp app-open ~/../usr/bin/
-echo "Copying app-open to ~/../usr/bin/ done"
